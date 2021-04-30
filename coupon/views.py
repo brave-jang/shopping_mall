74aa9256257302d2fa241f9e28f831a6423447a6
bravejang <wkdrmfmt@gmail.com>
2021-05-01 00:04:51 +0900

    coupon views.py 세팅 및 urls.py tpxld

--- coupon/views.py
+++ coupon/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.utils import timezone
+from django.views.decorators.http import require_POST
+from .models import Coupon
+from .forms import AddCouponForm
 
-# Create your views here.
+
+@require_POST
+def add_coupon(request):
+    now = timezone.now()
+    form = AddCouponForm(request.POST)
+    if form.is_valid():
+        code = form.cleaned_data['code']
+
+        try:
+            coupon = Coupon.objects.get(code__iexact=code, use_form__lte=now, use_to__gte=now, active=True) # iexcat : 대,소문자 가리지 않고 일치하는 문자
+            request.session['coupon_id'] = coupon.id
+        except Coupon.DoesNotExist:
+            request.session['coupon_id'] = None
+
+    return redirect('cart:detail')
