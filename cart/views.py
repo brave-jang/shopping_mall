e556176737181115432c6bfa6d9160cb6113ff3d
bravejang <wkdrmfmt@gmail.com>
2021-04-30 11:39:11 +0900

    views의 detail, remove 생성 및 urls.py 설정

--- cart/views.py
+++ cart/views.py
@@ -19,3 +19,17 @@ def add(request, product_id):
         cart.add(product=product, quantity=cd['quantity'], is_update=cd['is_update'])
 
         return redirect('cart:detail')
+        
+
+def remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:detail')
+
+def detail(request):
+    cart = Cart(request)
+    for product in cart:
+        product['quantity_form'] = AddProductForm(initial={'quantity':product['quantity'], 'is_update':True})
+
+    return render(request, 'cart/detail.html', {'cart':cart})
