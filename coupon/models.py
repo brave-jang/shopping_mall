8f6411f0079e1c5602616e71c85dc8f29d99f66a
bravejang <wkdrmfmt@gmail.com>
2021-05-01 00:31:24 +0900

    쿠폰 템플릿

--- coupon/models.py
+++ coupon/models.py
@@ -5,9 +5,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
     use_from = models.DateTimeField()
-    user_to = models.DateTimeField()
+    use_to = models.DateTimeField()
     amount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100000)])
-    active = models.BooleanField
+    active = models.BooleanField()
 
     def __str__(self):
         return self.code
--- coupon/views.py
+++ coupon/views.py
@@ -13,7 +13,7 @@ def add_coupon(request):
         code = form.cleaned_data['code']
 
         try:
-            coupon = Coupon.objects.get(code__iexact=code, use_form__lte=now, use_to__gte=now, active=True) # iexcat : 대,소문자 가리지 않고 일치하는 문자
+            coupon = Coupon.objects.get(code__iexact=code, use_from__lte=now, use_to__gte=now, active=True) # iexcat : 대,소문자 가리지 않고 일치하는 문자
             request.session['coupon_id'] = coupon.id
         except Coupon.DoesNotExist:
             request.session['coupon_id'] = None
