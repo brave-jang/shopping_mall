433aac1e3dc18f45ec321ce019baf04c09d1d6e4
bravejang <wkdrmfmt@gmail.com>
2021-04-30 11:24:09 +0900

    cart views.py 세팅

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .forms import AddProductForm
+from .cart import Cart
 
-# Create your views here.
+
+def add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+
+    # 클라이언트 -> 서버로 데이터 전달
+    # 유효성검사, injection 전처리
+    form = AddProductForm(request.POST)
+
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], is_update=cd['is_update'])
+
+        return redirect('cart:detail')
