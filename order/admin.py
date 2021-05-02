0ae521231baa5939cba21a076eb759adbe62943d
bravejang <wkdrmfmt@gmail.com>
2021-05-03 02:05:12 +0900

    관리자 페이지 엑셀로 다운

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,39 @@
+import csv
+import datetime
 from django.contrib import admin
+from django.http import HttpRequest
+from .models import Order, OrderItem
 
-# Register your models here.
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpRequest(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;filename={}.csv.format(opts.verbose_name)'
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%Y-%m-%d")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'    
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product'] # 검색 버튼
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline] 
+    actions = [export_to_csv]
+
